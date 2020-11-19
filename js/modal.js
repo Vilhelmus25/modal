6c558262494d035321413e6b76c481dd05372c27
Vörös Vilmos (Admin) <admin_vorosv@uni-bge.hu>
2020-11-19 15:55:49 +0100

    js hátra

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+let modal = document.querySelectorAll('.body__area');
+
+document.addEventListener('mouseover', () => {
+    document.getElementById("modal__id").style.display = 'block';               // getElementsByClassNames-re még nem működik
+});
+
