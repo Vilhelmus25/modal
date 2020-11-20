3a6bbce19f13047f858c62f649af95ae0421ec5f
Vörös Vilmos (Admin) <admin_vorosv@uni-bge.hu>
2020-11-20 13:50:22 +0100

    almost done

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,41 @@
-let modal = document.querySelectorAll('.body__area');
+'use strict';
 
-document.addEventListener('mouseover', () => {
-    document.getElementById("modal__id").style.display = 'block';               // getElementsByClassNames-re még nem működik
-});
+const modalEventListener = document.querySelector('.button__pressStart .search__btn .button__close .modal__button--okay .modal__button--cancel');
 
+// document.addEventListener('click', (event) => {
+//     event.currentTarget.getElementById("modal__id").style.display = 'flex';               // getElementsByClassNames-re még nem működik
+// });
+
+for (let i = 0; i < modalEventListener.length; i++) {
+    modalEventListener[i].addEventListener('click', toggleModal);
+
+}
+
+
+
+
+let modal = document.querySelector(".modal");
+// let triggerPressStart = document.querySelector('.button__pressStart');
+// let triggerSearch = document.querySelector('.search__btn');
+// let closeButton = document.querySelector(".button__close");
+// let okayButton = document.querySelector(".modal__button--okay");
+// let cancelButton = document.querySelector(".modal__button--cancel");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+
+modalEventListener.addEventListener("click", toggleModal);
+// triggerPressStart.addEventListener("click", toggleModal);
+// triggerSearch.addEventListener("click", toggleModal);
+// closeButton.addEventListener("click", toggleModal);
+// okayButton.addEventListener("click", toggleModal);
+// cancelButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
