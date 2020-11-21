31d87f8913564d71804a9533f9d090a03fc38f47
Vilhelmus25 <vorvie@gmail.com>
2020-11-21 22:17:05 +0100

    ready

--- js/modal.js
+++ js/modal.js
@@ -1,41 +1,22 @@
 'use strict';
 
-const modalEventListener = document.querySelector('.button__pressStart .search__btn .button__close .modal__button--okay .modal__button--cancel');
-
-// document.addEventListener('click', (event) => {
-//     event.currentTarget.getElementById("modal__id").style.display = 'flex';               // getElementsByClassNames-re még nem működik
-// });
+// így kell multiplét selectorozni (querySelectorAll, valamint felsorolva a class-ok ,-vel és whitespace-el elválasztva)
+const modalEventListener = document.querySelectorAll('.button__pressStart, .search__btn, .button__close, .modal__button--okay, .modal__button--cancel');
 
 for (let i = 0; i < modalEventListener.length; i++) {
-    modalEventListener[i].addEventListener('click', toggleModal);
-
+    modalEventListener[i].addEventListener('click', toggleModal);       // eseményfigyelőt teszünk rájuk, kattintásra ('click') és meghívjuk a toggleModalt
 }
 
-
-
-
-let modal = document.querySelector(".modal");
-// let triggerPressStart = document.querySelector('.button__pressStart');
-// let triggerSearch = document.querySelector('.search__btn');
-// let closeButton = document.querySelector(".button__close");
-// let okayButton = document.querySelector(".modal__button--okay");
-// let cancelButton = document.querySelector(".modal__button--cancel");
+let modal = document.querySelector(".modal");       // a modált is querySelectorozzuk
 
 function toggleModal() {
-    modal.classList.toggle("show-modal");
+    modal.classList.toggle("show-modal");       // a show-modal osztályban van az átállítás
 }
 
 function windowOnClick(event) {
-    if (event.target === modal) {
+    if (event.target === modal) {               // ha az ablakból kikattintunk akkor becsukja
         toggleModal();
     }
 }
 
-
-modalEventListener.addEventListener("click", toggleModal);
-// triggerPressStart.addEventListener("click", toggleModal);
-// triggerSearch.addEventListener("click", toggleModal);
-// closeButton.addEventListener("click", toggleModal);
-// okayButton.addEventListener("click", toggleModal);
-// cancelButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
